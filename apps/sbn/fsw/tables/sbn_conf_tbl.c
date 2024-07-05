a1b4117021fffcb627d50e982d61cf9cf3c770b8
Daniel Diamont <daniel.diamont@gmail.com>
2024-07-04 21:01:59 -0500

    sbn config

--- apps/sbn/fsw/tables/sbn_conf_tbl.c
+++ apps/sbn/fsw/tables/sbn_conf_tbl.c
@@ -16,31 +16,25 @@ SBN_ConfTbl_t SBN_ConfTbl = {.ProtocolModules = {{/* [0] */
 
                              .Peers =
                                  {
-                                     {/* [0] */
+                                    {/* [0] */
                                       .ProcessorID  = 1,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
                                       .ProtocolName = "UDP",
                                       .Filters      = {"Remap"},
                                       .Address      = "127.0.0.1:2234",
-                                      .TaskFlags    = SBN_TASK_POLL},
-                                     {/* [1] */
+                                      .TaskFlags    = SBN_TASK_POLL
+                                    },
+                                    {/* [1] */
                                       .ProcessorID  = 2,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
                                       .ProtocolName = "UDP",
                                       .Filters      = {"Remap"},
                                       .Address      = "127.0.0.1:2235",
-                                      .TaskFlags    = SBN_TASK_POLL},
-                                     {/* [2] */
-                                      .ProcessorID  = 3,
-                                      .SpacecraftID = 0x42,
-                                      .NetNum       = 0,
-                                      .ProtocolName = "UDP",
-                                      .Filters      = {"Remap"},
-                                      .Address      = "127.0.0.1:2236",
-                                      .TaskFlags    = SBN_TASK_POLL},
+                                      .TaskFlags    = SBN_TASK_POLL
+                                    },
                                  },
-                             .PeerCnt = 3};
+                             .PeerCnt = 2};
 
 CFE_TBL_FILEDEF(SBN_ConfTbl, SBN.SBN_ConfTbl, SBN Configuration Table, sbn_conf_tbl.tbl)
