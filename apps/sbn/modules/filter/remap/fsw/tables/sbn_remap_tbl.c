c63afa5523251b9acd8b94a2c138bfbc91b84ee1
Daniel Diamont <daniel.diamont@gmail.com>
2024-07-04 22:35:41 -0500

    remap

--- apps/sbn/modules/filter/remap/fsw/tables/sbn_remap_tbl.c
+++ apps/sbn/modules/filter/remap/fsw/tables/sbn_remap_tbl.c
@@ -3,9 +3,15 @@
 
 SBN_RemapTbl_t SBN_RemapTbl = {
     .RemapDefaultFlag = SBN_REMAP_DEFAULT_SEND,
-    .Entries          = {{.ProcessorID = 3, .SpacecraftID = 0x42, .FromMID = {0x0882}, .ToMID = {0x0883}},
+    .Entries          = {
+        {.ProcessorID = 2, .SpacecraftID = 0x42, .FromMID = {0x1806}, .ToMID = {0x1906}},
+        {.ProcessorID = 2, .SpacecraftID = 0x42, .FromMID = {0x1807}, .ToMID = {0x1907}},
 
-                /** ProcessorID "0" signals the end of the table. */
-                {.ProcessorID = 0, .SpacecraftID = 0, .FromMID = {0x0000}, .ToMID = {0x0000}}}}; /* end SBN_RemapTbl */
+        /** ProcessorID "0" signals the end of the table. */
+        {
+            .ProcessorID = 0, .SpacecraftID = 0, .FromMID = {0x0000}, .ToMID = {0x0000}
+        }
+    }
+}; /* end SBN_RemapTbl */
 
 CFE_TBL_FILEDEF(SBN_RemapTbl, SBN.SBN_RemapTbl, SBN Remap Table, sbn_remap_tbl.tbl)
